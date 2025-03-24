f09bca4729bcfce0332fa02eb20483635b1cd2b6
Rpallas1 <rpallas1@asu.edu>
2025-03-24 16:43:40 -0700

    Update rate limit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const notFoundMiddleware = require("./src/middleware/notFound");
 const errorHandlerMiddleware = require("./src/middleware/errorHandler");
 
 let limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
