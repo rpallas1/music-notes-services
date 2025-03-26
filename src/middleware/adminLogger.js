6bc086a5a6f1ebeb7cb41781fc5de3abd26757a6
Rpallas1 <rpallas1@asu.edu>
2025-03-25 18:32:21 -0700

    Fix adminLogger path

---
+++ src/middleware/adminLogger.js
@@ -0,0 +1,22 @@
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+
+morgan.token("response-body", (req, res) => {
+  return res.body ? JSON.stringify(res.body) : "";
+});
+const adminLogStream = fs.createWriteStream(
+  path.join(__dirname, "../..", "admin.log"),
+  {
+    flags: "a",
+  },
+);
+
+const adminLogger = morgan(
+  ":method :url :status :date[iso] :response-time ms - :res[content-length] :response-body :remote-addr",
+  {
+    stream: adminLogStream,
+  },
+);
+
+module.exports = morganLogger;
